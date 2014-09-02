4f92080d9adb20eb2e7d8cc07efd4e422a23aa97
Marina Murashev <marinamurashev@gmail.com>
2014-09-01 21:57:41 -0500

    adds vibration countdown to timer

--- src/com/example/hiittimer/TimerFragment.java
+++ src/com/example/hiittimer/TimerFragment.java
@@ -1,8 +1,13 @@
 package com.example.hiittimer;
 
+import java.util.Arrays;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +19,10 @@ public class TimerFragment extends Fragment {
 	CountDownTimer mSprintCountDownTimer;
 	private static int SECONDS_OF_REST = 10;
 	private static int SECONDS_OF_SPRINT = 20;
-
+	private static int VIBRATION_DURATION_MILLISECONDS = 400;
+	private static int[] SECONDS_LEFT_OF_REST_VIBRATIONS = { 1, 2, 3 };
+	private static int[] SECONDS_LEFT_OF_SPRINT_VIBRATIONS = { 1 };
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -26,19 +34,24 @@ public class TimerFragment extends Fragment {
 		
         
         mTimeRemainingText = (TextView) view.findViewById(R.id.time_left);
-        setRestCountDownTimer();
-        setSprintCountDownTimer();
+        Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        setRestCountDownTimer(vibrator);
+        setSprintCountDownTimer(vibrator);
         
         mRestCountDownTimer.start();
 		
 		return view;
 	}
 	
-	private void setRestCountDownTimer(){
+	private void setRestCountDownTimer(final Vibrator vibrator){
     	mRestCountDownTimer = new CountDownTimer(SECONDS_OF_REST * 1000, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                mTimeRemainingText.setText("rest seconds remaining: " + millisUntilFinished / 1000);
+            	long seconds_until_finished = millisUntilFinished / 1000;
+            	if(listContainsElement(SECONDS_LEFT_OF_REST_VIBRATIONS, (int) seconds_until_finished)){
+            		vibrator.vibrate(VIBRATION_DURATION_MILLISECONDS);
+            	}
+                mTimeRemainingText.setText("rest seconds remaining: " + seconds_until_finished);
             }
 
             public void onFinish() {
@@ -47,16 +60,32 @@ public class TimerFragment extends Fragment {
          };
     }
     
-    private void setSprintCountDownTimer(){
+    private void setSprintCountDownTimer(final Vibrator vibrator){
     	mSprintCountDownTimer = new CountDownTimer(SECONDS_OF_SPRINT * 1000, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                mTimeRemainingText.setText("sprint seconds remaining: " + millisUntilFinished / 1000);
+            	long seconds_until_finished = millisUntilFinished / 1000;
+            	
+            	if(listContainsElement(SECONDS_LEFT_OF_SPRINT_VIBRATIONS, (int) seconds_until_finished)){
+            		vibrator.vibrate(VIBRATION_DURATION_MILLISECONDS);
+            	}
+            	
+                mTimeRemainingText.setText("sprint seconds remaining: " + seconds_until_finished);
             }
 
             public void onFinish() {
                 mRestCountDownTimer.start();
             }
          };
+    }
+    
+    private boolean listContainsElement(int[] list, int element){
+    	for(int item : list){
+    		if (item == element){
+    			return true;
+    		}
+    	}
+    	
+    	return false;
     }
 }
