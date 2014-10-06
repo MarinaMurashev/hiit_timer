268fb68ff32ec29c0c2264fa22b8b93fe4d85c58
Marina Murashev <marinamurashev@gmail.com>
2014-10-06 17:47:32 -0500

    adds SettingsActivity, adds a .gitignore and deletes files that should be ignored

---
+++ app/src/main/java/com/example/hiittimer/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.hiittimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by thoughtworker on 10/6/14.
+ */
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
